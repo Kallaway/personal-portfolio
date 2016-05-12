6d673aaabec31fb3f786f22485b1c7165ec3016b
Alexander Kallaway <kallaway@me.com>
2016-05-11 21:01:10 -0400

    work in progress

---
+++ main.js
@@ -0,0 +1,10 @@
+// IDEAS
+// buttons change color on hover - do with css
+// buttons change color on click - do with css - change the right colors
+
+// TODO:
+// Two classes - solarized-dark and solarized-light
+// Choose a good font
+// Talk about skills? Very briefly
+// Talk about FreeCodeCamp
+// For the last section: Advice to contact me with jobs/resume/etc.
